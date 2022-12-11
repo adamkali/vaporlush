26e94fc43d0679ca971f65a910d04a426d22337a
adamkali <adamkali@outlook.com>
2022-12-10 19:07:05 -0500

    Update Lush to have correct configuration in colors/vaporlush.lua

--- colors/vaporlush.lua
+++ colors/vaporlush.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
 vim.opt.background = 'dark'
-vim.g.colors_name = 'lush_template'
+vim.g.colors_name = 'vaporlush'
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
@@ -14,7 +14,7 @@ vim.g.colors_name = 'lush_template'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.lush_template'] = nil
+package.loaded['vaporlush-theme.vaporlush'] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.lush_template'))
+require('lush')(require('vaporlush-theme.vaporlush'))
