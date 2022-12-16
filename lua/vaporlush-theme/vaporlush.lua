6eedf721a93586225199cec354d78f90fcbc45fa
Adam Kalinowski <Adam>
2022-12-16 17:12:56 -0500

    Add TreeSitter config

--- lua/vaporlush-theme/vaporlush.lua
+++ lua/vaporlush-theme/vaporlush.lua
@@ -28,22 +28,22 @@ local hsl = lush.hsl
 
 local VaporLush = {
     Primary = {
-        shade0 = hsl(250, 90, 10),
-        shade1 = hsl(250, 90, 40),
-        shade2 = hsl(250, 90, 60),
-        shade3 = hsl(250, 90, 80),
+        shade0 = hsl(250, 100, 10),
+        shade1 = hsl(250, 100, 30),
+        shade2 = hsl(250, 100, 50),
+        shade3 = hsl(250, 100, 80),
     },
     Secondary = {
-        shade0 = hsl(300, 90, 20),
-        shade1 = hsl(300, 90, 40),
-        shade2 = hsl(300, 90, 60),
-        shade3 = hsl(300, 90, 80),
+        shade0 = hsl(300, 100, 20),
+        shade1 = hsl(300, 100, 40),
+        shade2 = hsl(300, 100, 60),
+        shade3 = hsl(300, 100, 80),
     },
     Tertiary = {
-        shade0 = hsl(200, 90, 20),
-        shade1 = hsl(200, 90, 40),
-        shade2 = hsl(200, 90, 60),
-        shade3 = hsl(200, 90, 80),
+        shade0 = hsl(200, 90, 30),
+        shade1 = hsl(200, 90, 50),
+        shade2 = hsl(200, 90, 70),
+        shade3 = hsl(200, 90, 90),
     },
     Quartenary = {
         shade0 = hsl(275, 90, 20),
@@ -52,7 +52,7 @@ local VaporLush = {
         shade3 = hsl(275, 90, 80),
     },
     Quintary = {
-        shade0 = hsl(180, 100, 30),
+        shade0 = hsl(180, 100, 25),
         shade1 = hsl(180, 100, 40),
         shade2 = hsl(180, 100, 60),
         shade3 = hsl(180, 100, 90),
@@ -150,143 +150,88 @@ return lush(function(injected_functions)
             Question     { bg = VaporLush.Secondary.shade0,     fg = VaporLush.Secondary.shade2                     },      -- |hit-enter| prompt and yes/no questions
             Search       { bg = VaporLush.Tertiary.shade3,      fg = VaporLush.Primary.shade0                       }, 
             Visual       { bg = VaporLush.Secondary.shade0,     fg = VaporLush.Tertiary.shade3                      },      -- Visual mode selection
-
             Comment      { bg = VaporLush.Primary.shade0,       fg = VaporLush.Quintary.shade3,     gui=italic      },      -- Any comment
-
             Constant     { bg = Normal.bg,                      fg = VaporLush.Quintary.shade2                      },      -- (*) Any constant
             String       { bg = Normal.bg,                      fg = VaporLush.Quintary.shade1                      },      --   A string constant: "this is a string"
             Character    { String                                                                                   },      --   A character constant: 'c', '\n'
             Number       { bg = Normal.bg,                      fg = Constant.fg                                    },      --   A number constant: 234, 0xff
             Boolean      { Constant,                                                                gui=italic      },      --   A boolean constant: TRUE, false
             Float        { Number                                                                                   },      --   A floating point constant: 2.3e10
-
-            Identifier     { Constant,                                                              gui=""          },      -- (*) Any variable name
-            Function       { Constant,                                                              gui=italic      },      
-
-            Statement      { bg = VaporLush.Primary.shade0,     fg = VaporLush.Primary.shade2                       },      -- (*) Any statement
+            Identifier   { bg = Nomal.bg,                       fg=VaporLush.Secondary.shade1,      gui=""          },      -- (*) Any variable name
+            Function     { bg = Nomal.bg,                       fg=VaporLush.Quartenary.shade2,      gui=italic      },      
+            Statement      { bg = VaporLush.Primary.shade0,     fg = VaporLush.Primary.shade1,      gui=""          },      -- (*) Any statement
             Conditional    { Statement                                                                              },      --   if, then, else, endif, switch, etc.
             Repeat         { Statement,                                                             gui=italic      },      --   for, do, while, etc.
-            Label          { Statement,                                                             gui=italic      },      --   case, default, etc.
-            Operator       { Statement                                                                              },      --   "sizeof", "+", "*", etc.
-            Keyword        { Statement                                                                              },      --   any other keyword
-            Exception      { Repeat                                                                                 },      --   try, catch, throw
-
+            Label          { bg=VaporLush.Primary.shade0,       fg=VaporLush.Primary.shade2, gui=italic      },      --   case, default, etc.
+            Operator       { Label                                                                                  },      --   "sizeof", "+", "*", etc.
+            Keyword        { Label                                                                                  },      --   any other keyword
+            Exception      { Label                                                                                 },      --   try, catch, throw
             PreProc        { bg = Normal.bg,                    fg = VaporLush.Tertiary.shade2,     gui=italic      },      -- (*) Generic Preprocessor
             Include        { PreProc                                                                                },      --   Preprocessor #include
             Define         { PreProc                                                                                },      --   Preprocessor #define
             Macro          { PreProc                                                                                },      --   Same as Define
             PreCondit      { PreProc                                                                                },      --   Preprocessor #if, #else, #endif, etc.
-
-            Type           { bg = Normal.bg,                    fg = VaporLush.Secondary.shade2,    gui=italic      },      -- (*) int, long, char, etc.
+            Type           { bg = Normal.bg,                    fg = VaporLush.Quintary.shade0,    gui=italic      },      -- (*) int, long, char, etc.
             StorageClass   { Type,                                                                                  },      --   static, register, volatile, etc.
-            Structure      { Type,                                                                  gui=""          },      --   struct, union, enum, etc.
-            Typedef        { Type                                                                                   },      --   A typedef
-
+            Structure      { bg = Normal.bg,                    fg = VaporLush.Quartenary.shade1,   gui=italic      },      --   struct, union, enum, etc.
+            Typedef        { Structure                                                                              },      --   A typedef
             Special        { bg = Normal.bg,                    fg = VaporLush.Tertiary.shade3                      },      -- (*) Any special symbol
             SpecialChar    { Special                                                                                },      --   Special character in a constant
             Tag            { Special                                                                                },      --   You can use CTRL-] on this
             Delimiter      { Special                                                                                },      --   Character that needs attention
             SpecialComment { Special,                                                               gui = italic    },      
             Debug          { SpecialComment                                                                         },      --   Debugging statements
-
             Underlined     { bg= Normal.bg,                     fg= VaporLush.Secondary.shade1,     gui = underline },      -- Text that stands out, HTML links
             Ignore         { bg= VaporLush.Tertiary.shade0,     fg= VaporLush.Quartenary.shade2,    gui = italic    },      
             Error          { bg= VaporLush.Secondary.shade2,     fg= VaporLush.Quintary.shade3                      },      -- Any erroneous construct
             Todo           { bg= VaporLush.Quartenary.shade2,   fg= VaporLush.Quintary.shade3,      gui = underline }, 
 
-            --
-            -- LspReferenceText            { } , -- Used for highlighting "text" references
-            -- LspReferenceRead            { } , -- Used for highlighting "read" references
-            -- LspReferenceWrite           { } , -- Used for highlighting "write" references
-            -- LspCodeLens                 { } , -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
-            -- LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
-            -- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
 
-            -- See :h diagnostic-highlights, some groups may not be listed, submit a PR fix to lush-template!
-            --
-            -- DiagnosticError            { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-            -- DiagnosticWarn             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-            -- DiagnosticInfo             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-            -- DiagnosticHint             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-            -- DiagnosticVirtualTextError { } , -- Used for "Error" diagnostic virtual text.
-            -- DiagnosticVirtualTextWarn  { } , -- Used for "Warn" diagnostic virtual text.
-            -- DiagnosticVirtualTextInfo  { } , -- Used for "Info" diagnostic virtual text.
-            -- DiagnosticVirtualTextHint  { } , -- Used for "Hint" diagnostic virtual text.
-            -- DiagnosticUnderlineError   { } , -- Used to underline "Error" diagnostics.
-            -- DiagnosticUnderlineWarn    { } , -- Used to underline "Warn" diagnostics.
-            -- DiagnosticUnderlineInfo    { } , -- Used to underline "Info" diagnostics.
-            -- DiagnosticUnderlineHint    { } , -- Used to underline "Hint" diagnostics.
-            -- DiagnosticFloatingError    { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
-            -- DiagnosticFloatingWarn     { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
-            -- DiagnosticFloatingInfo     { } , -- Used to color "Info" diagnostic messages in diagnostics float.
-            -- DiagnosticFloatingHint     { } , -- Used to color "Hint" diagnostic messages in diagnostics float.
-            -- DiagnosticSignError        { } , -- Used for "Error" signs in sign column.
-            -- DiagnosticSignWarn         { } , -- Used for "Warn" signs in sign column.
-            -- DiagnosticSignInfo         { } , -- Used for "Info" signs in sign column.
-            -- DiagnosticSignHint         { } , -- Used for "Hint" signs in sign column.
-
-            -- Tree-Sitter syntax groups.
-            --
-            -- See :h treesitter-highlight-groups, some groups may not be listed,
-            -- submit a PR fix to lush-template!
-            --
-            -- Tree-Sitter groups are defined with an "@" symbol, which must be
-            -- specially handled to be valid lua code, we do this via the special
-            -- sym function. The following are all valid ways to call the sym function,
-            -- for more details see https://www.lua.org/pil/5.html
-            --
-            -- sym("@text.literal")
-            -- sym('@text.literal')
-            -- sym"@text.literal"
-            -- sym'@text.literal'
-            --
-            -- For more information see https://github.com/rktjmp/lush.nvim/issues/109
-
-            -- sym"@text.literal"      { }, -- Comment
-            -- sym"@text.reference"    { }, -- Identifier
-            -- sym"@text.title"        { }, -- Title
-            -- sym"@text.uri"          { }, -- Underlined
-            -- sym"@text.underline"    { }, -- Underlined
-            -- sym"@text.todo"         { }, -- Todo
-            -- sym"@comment"           { }, -- Comment
-            -- sym"@punctuation"       { }, -- Delimiter
-            -- sym"@constant"          { }, -- Constant
-            -- sym"@constant.builtin"  { }, -- Special
-            -- sym"@constant.macro"    { }, -- Define
-            -- sym"@define"            { }, -- Define
-            -- sym"@macro"             { }, -- Macro
-            -- sym"@string"            { }, -- String
-            -- sym"@string.escape"     { }, -- SpecialChar
-            -- sym"@string.special"    { }, -- SpecialChar
-            -- sym"@character"         { }, -- Character
-            -- sym"@character.special" { }, -- SpecialChar
-            -- sym"@number"            { }, -- Number
-            -- sym"@boolean"           { }, -- Boolean
-            -- sym"@float"             { }, -- Float
-            -- sym"@function"          { }, -- Function
-            -- sym"@function.builtin"  { }, -- Special
-            -- sym"@function.macro"    { }, -- Macro
-            -- sym"@parameter"         { }, -- Identifier
-            -- sym"@method"            { }, -- Function
-            -- sym"@field"             { }, -- Identifier
-            -- sym"@property"          { }, -- Identifier
-            -- sym"@constructor"       { }, -- Special
-            -- sym"@conditional"       { }, -- Conditional
-            -- sym"@repeat"            { }, -- Repeat
-            -- sym"@label"             { }, -- Label
-            -- sym"@operator"          { }, -- Operator
-            -- sym"@keyword"           { }, -- Keyword
-            -- sym"@exception"         { }, -- Exception
-            -- sym"@variable"          { }, -- Identifier
-            -- sym"@type"              { }, -- Type
-            -- sym"@type.definition"   { }, -- Typedef
-            -- sym"@storageclass"      { }, -- StorageClass
-            -- sym"@structure"         { }, -- Structure
-            -- sym"@namespace"         { }, -- Identifier
-            -- sym"@include"           { }, -- Include
-            -- sym"@preproc"           { }, -- PreProc
-            -- sym"@debug"             { }, -- Debug
-            -- sym"@tag"               { }, -- Tag
+            sym"@text.literal"      { Comment }, -- Comment
+            sym"@text.reference"    { Identifier }, -- Identifier
+            sym"@text.title"        { Identifier }, -- Title
+            sym"@text.uri"          { Underlined }, -- Underlined
+            sym"@text.underline"    { Underlined }, -- Underlined
+            sym"@text.todo"         { Todo }, -- Todo
+            sym"@comment"           { Comment }, -- Comment
+            sym"@punctuation"       { Delimiter }, -- Delimiter
+            sym"@constant"          { Constant }, -- Constant
+            sym"@constant.builtin"  { Special }, -- Special
+            sym"@constant.macro"    { Define }, -- Define
+            sym"@define"            { Define }, -- Define
+            sym"@macro"             { Define }, -- Macro
+            sym"@string"            { String }, -- String
+            sym"@string.escape"     { SpecialChar }, -- SpecialChar
+            sym"@string.special"    { SpecialChar }, -- SpecialChar
+            sym"@character"         { Character }, -- Character
+            sym"@character.special" { SpecialChar }, -- SpecialChar
+            sym"@number"            { Number }, -- Number
+            sym"@boolean"           { Boolean }, -- Boolean
+            sym"@float"             { Float }, -- Float
+            sym"@function"          { Function }, -- Function
+            sym"@function.builtin"  { Special }, -- Special
+            sym"@function.macro"    { Macro }, -- Macro
+            sym"@parameter"         { Identifier }, -- Identifier
+            sym"@method"            { Function }, -- Function
+            sym"@field"             { Identifier }, -- Identifier
+            sym"@property"          { Identifier }, -- Identifier
+            sym"@constructor"       { bg=Normal.bg,  fg=VaporLush.Quintary.shade0 }, -- Special
+            sym"@conditional"       { Conditional }, -- Conditional
+            sym"@repeat"            { Repeat }, -- Repeat
+            sym"@label"             { Label }, -- Label
+            sym"@operator"          { Operator }, -- Operator
+            sym"@keyword"           { Keyword }, -- Keyword
+            sym"@exception"         { Exception }, -- Exception
+            sym"@variable"          { bg=Normal.bg, fg=VaporLush.Primary.shade3 }, -- Identifier
+            sym"@type"              { Type }, -- Type
+            sym"@type.definition"   { Typedef }, -- Typedef
+            sym"@storageclass"      { StorageClass }, -- StorageClass
+            sym"@structure"         { Structure }, -- Structure
+            sym"@namespace"         { Structure }, -- Identifier
+            sym"@include"           { Include }, -- Include
+            sym"@preproc"           { PreProc }, -- PreProc
+            sym"@debug"             { Debug }, -- Debug
+            sym"@tag"               { Tag }, -- Tag
         }
     end
 )
